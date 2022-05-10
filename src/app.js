75d4acb19491b41f70779940b72c4399cbd53d58
Arthur Gartner <arthur.gartner@gmail.com>
2022-05-09 20:21:03 -0400

    Added module to help with reg queries

--- src/app.js
+++ src/app.js
@@ -6,19 +6,13 @@ import { ipcRenderer } from "electron";
 import jetpack from "fs-jetpack";
 import { greet } from "./hello_world/hello_world";
 import env from "env";
-
-document.querySelector("#app").style.display = "block";
-document.querySelector("#greet").innerHTML = greet();
-document.querySelector("#env").innerHTML = env.name;
-document.querySelector("#electron-version").innerHTML =
-  process.versions.electron;
+import registry from "windows/lib/registry";
 
 const osMap = {
   win32: "Windows",
   darwin: "macOS",
-  linux: "Linux"
+  linux: "Linux",
 };
-document.querySelector("#os").innerHTML = osMap[process.platform];
 
 // We can communicate with main process through messages.
 ipcRenderer.on("app-path", (event, appDirPath) => {
@@ -26,15 +20,23 @@ ipcRenderer.on("app-path", (event, appDirPath) => {
   // files from disk like it's node.js! Welcome to Electron world :)
   const appDir = jetpack.cwd(appDirPath);
   const manifest = appDir.read("package.json", "json");
-  document.querySelector("#author").innerHTML = manifest.author;
 });
 ipcRenderer.send("need-app-path");
 
-document.querySelector(".electron-website-link").addEventListener(
+document.querySelector("#regLoad").addEventListener(
   "click",
-  event => {
-    ipcRenderer.send("open-external-link", event.target.href);
+  (event) => {
+    ipcRenderer.send("test", event.target.href);
     event.preventDefault();
   },
   false
+);
+
+var regedit = require("regedit");
+
+regedit.list(
+  "HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall",
+  function (err, result) {
+    console.log(result);
+  }
 );
